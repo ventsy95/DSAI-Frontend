e74a5ec6a7855e9921c3946c7ff9a27d69810fd2
Ventsy Mihalov <ventsy95@mail.bg>
2019-06-11 20:39:51 +0300

    Added missing files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { InputsModule } from '@progress/kendo-angular-inputs';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { GridModule } from '@progress/kendo-angular-grid';
+import { ButtonsModule } from '@progress/kendo-angular-buttons';
+import { TestComponent } from './test/test.component';
+import { GaugesModule } from '@progress/kendo-angular-gauges';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    TestComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    InputsModule,
+    BrowserAnimationsModule,
+    GridModule,
+    ButtonsModule,
+    GaugesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
